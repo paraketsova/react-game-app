509852067ebb015c729fdd152994ea60685412f8
Mariia Paraketsova <paraketsova@gmail.com>
2023-01-11 18:55:17 +0100

    Refactor Table - add props to components

--- src/App.js
+++ src/App.js
@@ -2,13 +2,31 @@ import React, { Component } from 'react';
 import Table from './Table';
 
 class App extends Component {
-  render () {
+  render() {
+    const characters = [
+      {
+        name: 'Charlie',
+        job: 'Janitor',
+      },
+      {
+        name: 'Mac',
+        job: 'Bouncer',
+      },
+      {
+        name: 'Dee',
+        job: 'Aspring actress',
+      },
+      {
+        name: 'Dennis',
+        job: 'Bartender',
+      },
+    ]
+
     return (
-      <div className="container" >
-        <Table />
+      <div className="container">
+        <Table characterData={characters} />
       </div>
     )
   }
 }
-
 export default App;
