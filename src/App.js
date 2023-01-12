8d1e651a628261ab5bb93ea6710a4efe8e5ec845
Mariia Paraketsova <paraketsova@gmail.com>
2023-01-12 15:46:00 +0100

    Submitting Form Data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import React, { Component } from 'react';
 import Table from './Table';
+import Form from './Form'
+
 
 class App extends Component {
-  state ={
-    characters: [
-      {
-        name: 'Charlie',
-        job: 'Janitor',
-      },
-      {
-        name: 'Mac',
-        job: 'Bouncer',
-      },
-      {
-        name: 'Dee',
-        job: 'Aspring actress',
-      },
-      {
-        name: 'Dennis',
-        job: 'Bartender',
-      },
-    ]
+  state = {
+    characters: [],
   }
 
   removeCharacter = (index) => {
@@ -33,12 +18,17 @@ class App extends Component {
     })
   }
 
+  handleSubmit = (character) => {
+    this.setState({ characters: [...this.state.characters, character] }) // add new character
+  }
+
   render() {
     const { characters } = this.state
 
     return (
       <div className="container">
         <Table characterData={characters} removeCharacter={this.removeCharacter} />
+        <Form handleSubmit={this.handleSubmit} />
       </div>
     )
   }
